8cdd005b014ab38c7dedb17d0ae50d6606c41d93
timlac <tim@lachmann.nu>
2022-09-26 12:24:35 +0200

    video js example

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import VideoPage from "./VideoPage";
 import UserForm from "./UserForm";
 
 import { UserContext } from "./Context";
+import VideoExample from "./VideoExample";
 
 export default function App() {
 
@@ -22,6 +23,7 @@ export default function App() {
                 <Routes>
                     <Route path="/" element={<UserForm />} />
                     <Route path="/VideoPage" element={<VideoPage />} />
+                    <Route path="/VideoExample" element={<VideoExample />} />
                 </Routes>
             </BrowserRouter>
         </UserContext.Provider>
