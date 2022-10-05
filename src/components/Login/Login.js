f7612232b57f2d55a07eafa897d66abb19bc17b6
timlac <tim@lachmann.nu>
2022-10-05 15:14:06 +0200

    implemented pretty login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,52 +1,102 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {Alert, Button, Checkbox, Form, Input, Space, Spin} from 'antd';
+import React from 'react';
+import './Login.css'
+
 
 async function verifyUser() {
     return fetch('https://1v3k9pr4el.execute-api.eu-west-1.amazonaws.com/items')
         .then(data => data.json())
 }
 
-export default function Login({setToken}) {
 
-    const [username, setUserName] = useState();
+export default function Login({setToken})  {
+
     const [alert, setAlert] = useState(false)
+    const [checking, setChecking] = useState(false)
 
-    const handleSubmit = async e => {
-        e.preventDefault()
+    useEffect(() => {
+        if(checking) {
+            setAlert(false)
+        }
+    }, [checking])
+
+
+    const onFinish = async (values) => {
+        setChecking(true)
         const data = await verifyUser()
         const items = data["Items"]
         const unique = [...new Set(items.map(item => item.alias))];
 
-        if (unique.includes(username)){
+        console.log(values.username)
+
+        if (unique.includes(values.username)){
+            setChecking(false)
             console.log("setting username")
-            console.log(username)
-            setToken({"token": username})
+            console.log(values.username)
+            setToken({"token": values.username})
         } else {
+            setChecking(false)
             setAlert(true)
         }
-    }
+    };
 
-    return(
-        <div className="login-wrapper">
-            <h1>Please Log In</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <p>Username</p>
-                    <input type="text" onChange={e => setUserName(e.target.value)}/>
-                </label>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-            {alert && <h2> Invalid username </h2>}
-        </div>
-    )
-}
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
 
+    return (
+        <div className="loginForm">
+        <Form
+            name="basic"
+            labelCol={{
+                span: 8,
+            }}
+            wrapperCol={{
+                span: 16,
+            }}
+            initialValues={{
+                remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your username!',
 
+                    },
+                ]}
+            >
+                <Input />
+            </Form.Item>
 
-const handle = async () => {
-    const data = await verifyUser()
-    console.log(data)
-}
-
-handle()
+            <Form.Item
+                wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                }}
+            >
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+                {checking &&
+                  <div className="spinner">
+                    <Spin />
+                  </div>
+            }
+                {alert &&  <Alert
+                              message="Invalid username"
+                              description="The username you've entered appears to be invalid"
+                              type="error"
+                            />}
+            </Form.Item>
+        </Form>
+            </div>
+    );
+};
